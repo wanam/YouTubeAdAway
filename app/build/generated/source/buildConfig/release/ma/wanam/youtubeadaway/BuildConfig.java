5539798c3e773c2faebed943e8a9cc36c7e7f31c
Christoph Pech <christoph.pech@gmail.com>
2018-09-26 10:30:10 +0200

    android studio update

--- app/build/generated/source/buildConfig/release/ma/wanam/youtubeadaway/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/ma/wanam/youtubeadaway/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "ma.wanam.youtubeadaway";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 380;
-  public static final String VERSION_NAME = "3.8.0";
+  public static final int VERSION_CODE = 432;
+  public static final String VERSION_NAME = "4.3.2";
 }
